515fcdab9b8d8e10ce541eaf1820a7172dd3d98e
opopipp458 <abc0980815912@gmail.com>
2026-03-31 20:20:48 +0700

    Create CalculatorTest.php

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,16 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Calculator;
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(5, $calc->add(2, 3));
+    }
+    public function testSubtract()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(1, $calc->subtract(3, 2));
+    }
+}
